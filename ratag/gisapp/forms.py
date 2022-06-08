a5be4515fca29a0bdded74e12c5259ef95ced3a2
eilam <eilam.elimelech@kyndryl.com>
2022-06-08 11:00:06 +0300

    second

--- ratag/gisapp/forms.py
+++ ratag/gisapp/forms.py
@@ -10,5 +10,5 @@ class NewHazard(forms.ModelForm):
 
         widgets = {
             'point': forms.OSMWidget(attrs={'default_lat': 32, 'default_lon': 35, 'map_width': 800, 'map_height': 500}),
-            'created_time': forms.SelectDateWidget()
+            'created_time22': forms.SelectDateWidget()
         }
